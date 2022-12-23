fba00bda2960c24ce626a9fe3207e48992fb6a2e
Iandayy <104152583+Iandayy@users.noreply.github.com>
2022-12-23 14:05:30 +0900

    Update request.js

--- client/src/service/request.js
+++ client/src/service/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "https://port-0-everyone-s-post-dpuqy925lbnnny33.gksl2.cloudtype.app/",
+    "https://port-0-server-dpuqy925lbnnny33.gksl2.cloudtype.app/",
   headers: { withCredentials: true },
 });
 
