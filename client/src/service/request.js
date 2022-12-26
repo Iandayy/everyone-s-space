47c69d9ae403019415cdbde45438e91c1b405fc0
Iandayy <104152583+Iandayy@users.noreply.github.com>
2022-12-26 21:23:28 +0900

    Update request.js

--- client/src/service/request.js
+++ client/src/service/request.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://port-0-server-dpuqy925lbnnny33.gksl2.cloudtype.app/",
-  withCredentials: true,
 });
 
 export default instance;
