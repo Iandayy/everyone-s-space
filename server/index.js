40f13a8b25c2bb4c263f020048980c3e1e7150eb
ian <onlordev@gmail.com>
2023-01-02 15:24:57 +0900

    add proxy

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const methodOverride = require("method-override");
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const post = require("./router/post");
 const auth = require("./router/auth");
@@ -22,7 +23,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://web-client-dpuqy925lbnnny33.gksl2.cloudtype.app/",
-      "https://everyone-s-post.vercel.app/"
+      "https://everyone-s-post.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
@@ -32,6 +33,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser("secret"));
 app.use(methodOverride("_method"));
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "https://everyone-s-post.vercel.app/",
+    changeOrigin: true,
+  })
+);
 
 app.use("/posts", post);
 app.use("/auth", auth);
