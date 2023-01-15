c1ed1b256a3f4ed3a5ccf6fc17fbaeae5491d1e0
Iandayy <104152583+Iandayy@users.noreply.github.com>
2023-01-15 18:09:54 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ mongoose
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser("secret"));
+app.use(cookieParser());
 app.use(methodOverride("_method"));
 
 app.use(
