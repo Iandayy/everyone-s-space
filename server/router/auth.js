86b72d0a3830b90f308dc70b7570e3acac5deb64
Iandayy <104152583+Iandayy@users.noreply.github.com>
2023-01-15 15:05:51 +0900

    Update auth.js

--- server/router/auth.js
+++ server/router/auth.js
@@ -48,8 +48,8 @@ router.post("/login", async (req, res) => {
       return;
     }
     const options = {
-      sameSite: "None",
-      secure: true,
+//       sameSite: "None",
+//       secure: true,
       maxAge: 10 * 60 * 1000,
     };
     if (user.name === req.body.name && user.password === password) {
