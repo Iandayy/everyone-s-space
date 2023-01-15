d6494b7a01319343ce08bf06839813fea6d066f2
ian <onlordev@gmail.com>
2023-01-15 11:53:48 +0900

    fix: api request

--- client/src/service/request.js
+++ client/src/service/request.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://port-0-server-dpuqy925lbnnny33.gksl2.cloudtype.app/",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default instance;
