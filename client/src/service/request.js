6eee4bcec9c1a29f9be0a7fce73b9f868e9110ad
Iandayy <104152583+Iandayy@users.noreply.github.com>
2023-01-15 17:42:46 +0900

    Update request.js

--- client/src/service/request.js
+++ client/src/service/request.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://port-0-server-dpuqy925lbnnny33.gksl2.cloudtype.app",
   withCredentials: true,
-  credentials: 'include',
   headers: {
     "Content-Type": "application/json",
   },
