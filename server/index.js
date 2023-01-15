bcf915c881af4c21e5b5e59797ae56415645f07c
Iandayy <104152583+Iandayy@users.noreply.github.com>
2023-01-15 10:09:09 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,17 +17,22 @@ mongoose
   .then(() => console.log("Open"))
   .catch((err) => console.log("Error", err));
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://web-client-dpuqy925lbnnny33.gksl2.cloudtype.app/",
-      "https://everyone-s-post.vercel.app/",
-    ],
-    credentials: true,
+const whitelist = ["http://localhost:3000", "https://web-client-dpuqy925lbnnny33.gksl2.cloudtype.app/","https://everyone-s-post.vercel.app/",];
+ 
+  const corsOptions = {
+    origin:  (origin, callback) => { 
+      if (whitelist.indexOf(origin) !== -1) { // 만일 whitelist 배열에 origin인자가 있을 경우
+        callback(null, true); // cors 허용
+      } else {
+        callback(new Error("Not Allowed Origin!")); // cors 비허용
+      }
+    },
+    credentials: origin && true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
-  })
-);
+  };
+   
+  app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser("secret"));
