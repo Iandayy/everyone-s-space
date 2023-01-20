3d7d5692e9779d2ba86d8d3f59482114092a4ffd
ian <onlordev@gmail.com>
2023-01-20 12:56:48 +0900

    fix: api url

--- client/src/containers/Time.jsx
+++
@@ -1,9 +0,0 @@
-const Time = () => {
-  return (
-    <form action="/extend" method="POST" className="mr-2 text-gray-500">
-      <button>extend</button>
-    </form>
-  );
-};
-
-export default Time;
