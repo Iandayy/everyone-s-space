0b8e65830ca298ac960370ee0ed921b03da6e02b
ian <onlordev@gmail.com>
2023-03-29 16:13:49 +0900

    style: footer

--- client/src/containers/Footer.jsx
+++ client/src/containers/Footer.jsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer className="m-5 border-t-2">feat.ian</footer>;
+  return (
+    <footer className="text-center text-gray-500 bg-gray-100 p-2">
+      ©2023. Everyone's Post. All rights reserved. feat.ian
+    </footer>
+  );
 };
 
 export default Footer;
